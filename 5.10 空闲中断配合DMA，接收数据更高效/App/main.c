5bac948c6df55657db0b5e442fb4a2516d0d771b
zdh <2579941211@qq.com>
2024-01-14 02:04:36 +0800

    找半天bug发现驱动文件放成老版本的了

--- 5.10 空闲中断配合DMA，接收数据更高效/App/main.c
+++ 5.10 空闲中断配合DMA，接收数据更高效/App/main.c
@@ -1,20 +1,14 @@
-/*
- * @Author: zdh 2579941211@qq.com
- * @Date: 2024-01-12 01:12:32
- * @LastEditors: zdh 2579941211@qq.com
- * @LastEditTime: 2024-01-12 02:13:28
- * @FilePath: \5.4 如何使用串口�?�?接收数据\App\main.c
- * @Description: 这是默�?��?�置,请�?�置`customMade`, 打开koroFileHeader查看配置 进�?��?�置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "gd32f30x.h"
 #include "led_drv.h"
 #include "key_drv.h"
-#include "usb2Com_drv.h"
+#include "systick.h"
+#include "usb2com_drv.h"
 
 int main(void)
-{
+{	
+	SystickInit();
 	LedDrvInit();
 	KeyDrvInit();
 	Usb2ComDrvInit();
--- 5.10 空闲中断配合DMA，接收数据更高效/Objects/main.d
+++ 5.10 空闲中断配合DMA，接收数据更高效/Objects/main.d
@@ -1,6 +1,6 @@
 .\objects\main.o: App\main.c
-.\objects\main.o: D:\Study\keil\UV4\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: D:\Study\keil\UV4\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: D:\Study\keil\UV4\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: .\Arm\gd32f30x.h
 .\objects\main.o: .\Arm\core_cm4.h
 .\objects\main.o: .\Arm\core_cmInstr.h
@@ -35,4 +35,5 @@
 .\objects\main.o: .\GD32_Hal\Include\gd32f30x_exmc.h
 .\objects\main.o: .\Driver\led_drv.h
 .\objects\main.o: .\Driver\key_drv.h
-.\objects\main.o: .\Driver\usb2Com_drv.h
+.\objects\main.o: .\Driver\systick.h
+.\objects\main.o: .\Driver\usb2com_drv.h
