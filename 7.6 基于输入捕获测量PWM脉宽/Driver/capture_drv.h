e9678bb7caf77a4a8ad06c54cf7b0807171a78bf
zdh <2579941211@qq.com>
2024-01-15 19:27:43 +0800

    定时器完结,需要进阶的话可以先把外设学完后再去结合开发手册，进行开发进阶

---
+++ 7.6 基于输入捕获测量PWM脉宽/Driver/capture_drv.h
@@ -0,0 +1,8 @@
+#ifndef __CAPTURE_DRV_H_
+#define __CAPTURE_DRV_H_
+
+void CaptureDrvInit(void);
+
+void CaptureDrvTest(void);
+
+#endif
---
+++ 7.6 基于输入捕获测量PWM脉宽/Driver/pwm_drv.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: zdh 2579941211@qq.com
+ * @Date: 2024-01-14 22:11:41
+ * @LastEditors: zdh 2579941211@qq.com
+void PwmDrvTest(void)
+ * @LastEditTime: 2024-01-14 23:03:26
+ * @FilePath: \7.2 基于PWM实现呼吸灯\Driver\pwm_drv.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef __PWM_DRV_H_
+#define __PWM_DRV_H_
+
+void PwmDrvInit(void);
+void PwmDrvTest(void);
+#endif
