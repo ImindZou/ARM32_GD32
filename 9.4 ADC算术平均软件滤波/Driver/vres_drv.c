aae09f2f233f354fde1c30dd88cf82d5fe2e7f1a
zdh <2579941211@qq.com>
2024-01-18 21:36:21 +0800

    做对比的

---
+++ 9.4 ADC算术平均软件滤波/Driver/vres_drv.c
@@ -0,0 +1,112 @@
+/*
+ * @Author: zdh 2579941211@qq.com
+ * @Date: 2024-01-18 16:19:57
+ * @LastEditors: zdh 2579941211@qq.com
+ * @LastEditTime: 2024-01-18 21:15:04
+ * @FilePath: \9.2 ADC???????\Driver\vres_drv.c
+ * @Description: ??????,???`customMade`, ??koroFileHeader???? ????: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdint.h>
+#include "gd32f30x.h"
+#include "delay.h"
+#include <stdio.h>
+
+static void GpioInit(void)
+{
+    rcu_periph_clock_enable(RCU_GPIOC);
+    gpio_init(GPIOC,GPIO_MODE_AIN,GPIO_OSPEED_10MHZ,GPIO_PIN_2);
+    gpio_init(GPIOC,GPIO_MODE_AIN,GPIO_OSPEED_10MHZ,GPIO_PIN_3);
+}
+
+static void AdcInit(void)
+{
+    /*使能时钟*/
+    rcu_periph_clock_enable(RCU_ADC0);
+    /*设置分频系数*/
+    rcu_adc_clock_config(RCU_CKADC_CKAPB2_DIV6);    //8???120MHz / 6 = 20 MHz
+    /*设置独立模式*/ 
+    adc_mode_config(ADC_MODE_FREE);
+    /*设置连续模式*/ 
+    adc_special_function_config(ADC0,ADC_CONTINUOUS_MODE,ENABLE);   //continue convert mode
+     /*设置扫描模式*/ 
+    adc_special_function_config(ADC0,ADC_SCAN_MODE,ENABLE);   //Scan Mode
+    /*设置数据对齐*/ 
+    adc_data_alignment_config(ADC0,ADC_DATAALIGN_RIGHT);
+    /*设置转换通道个数*/
+    adc_channel_length_config(ADC0,ADC_REGULAR_CHANNEL,2);
+    /*设置转换哪一个通道以及所处序列位置*/ 
+    adc_regular_channel_config(ADC0,0,ADC_CHANNEL_12,ADC_SAMPLETIME_239POINT5); //PC2????12?????????0????239.5???
+    adc_regular_channel_config(ADC0,1,ADC_CHANNEL_13,ADC_SAMPLETIME_239POINT5); //PC2????12?????????0????239.5???
+    /*设置选择哪一个外部触发源*/
+    adc_external_trigger_source_config(ADC0,ADC_REGULAR_CHANNEL,ADC0_1_2_EXTTRIG_REGULAR_NONE);
+    /*使能外部触发*/
+    adc_external_trigger_config(ADC0,ADC_REGULAR_CHANNEL,ENABLE);
+    /*使能ADC的DMA功能*/
+    adc_dma_mode_enable(ADC0);
+    /*使能ADC*/
+    adc_enable(ADC0);
+    /*内部校准*/
+    DelayNus(50);
+    adc_calibration_enable(ADC0);
+    adc_software_trigger_enable(ADC0,ADC_REGULAR_CHANNEL);
+
+}
+
+#define ADC0_RDATA_ADDR     (ADC0 + 0x4C);
+static uint16_t g_adcVal[2];
+
+static void DmaInit(void)
+{
+	/* 使能DMA时钟；*/
+	rcu_periph_clock_enable(RCU_DMA0);
+	/* 复位DMA通道；*/
+	dma_deinit(DMA0,DMA_CH0);
+ 
+	dma_parameter_struct dmaStruct;
+    dma_struct_para_init(&dmaStruct);
+	/* 配置传输方向；*/ 
+	dmaStruct.direction = DMA_PERIPHERAL_TO_MEMORY;
+	/* 配置数据源地址；*/ 
+	dmaStruct.periph_addr = ADC0_RDATA_ADDR;
+	/* 配置源地址是固定的还是增长的；*/ 
+	dmaStruct.periph_inc = DMA_PERIPH_INCREASE_DISABLE;
+	/* 配置源数据传输位宽；*/ 
+	dmaStruct.periph_width = DMA_PERIPHERAL_WIDTH_16BIT;
+	
+	/* 配置数据目的地址；*/
+	dmaStruct.memory_addr = (uint32_t)g_adcVal;
+	/* 配置目的地址是固定的还是增长的；*/ 
+	dmaStruct.memory_inc = DMA_MEMORY_INCREASE_ENABLE;
+	/* 配置目的数据传输位宽；*/ 
+	dmaStruct.memory_width = DMA_MEMORY_WIDTH_16BIT;
+	/* 配置数据传输最大次数；*/ 
+	dmaStruct.number = 2;
+	/* 配置DMA通道优先级；*/ 
+	dmaStruct.priority = DMA_PRIORITY_HIGH;
+	dma_init(DMA0,DMA_CH0, &dmaStruct);
+	
+	/* 使能UART接收数据使用DMA；*/ 
+	dma_circulation_enable(DMA0,DMA_CH0);
+	/* 使能DMA通道；*/ 
+	dma_channel_enable(DMA0, DMA_CH0);
+}
+/**
+***************************************
+*@brief ADC硬件初始化
+*@param 
+*@return 
+***************************************
+*/
+void VresDrvInit(void)
+{
+    GpioInit();
+    AdcInit();
+    DmaInit();
+}
+
+void VresDrvTest(void)
+{
+    printf("CH12 AdcVal1 = %d.\n", g_adcVal[0]);
+    printf("CH13 AdcVal1 = %d.\n", g_adcVal[1]);
+    DelayNms(1000);
+}
