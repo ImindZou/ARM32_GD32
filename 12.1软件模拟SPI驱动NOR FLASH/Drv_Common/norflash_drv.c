a9a2ca6805b1c9a7e7390fecfb8f6b37dcad00ea
zdh <2579941211@qq.com>
2024-02-02 17:04:56 +0800

    软件模拟SPI最终版本

--- 12.1软件模拟SPI驱动NOR FLASH/Drv_Common/norflash_drv.c
+++ 12.1软件模拟SPI驱动NOR FLASH/Drv_Common/norflash_drv.c
@@ -2,15 +2,7 @@
  * @Author: zdh 2579941211@qq.com
  * @Date: 2024-02-02 13:51:12
  * @LastEditors: zdh 2579941211@qq.com
- * @LastEditTime: 2024-02-02 14:50:58
- * @FilePath: \12.1软件模拟SPI驱动NOR FLASH\Drv_Common\norflash_drv.c
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
- * @Author: zdh 2579941211@qq.com
- * @Date: 2024-02-02 13:51:12
- * @LastEditors: zdh 2579941211@qq.com
- * @LastEditTime: 2024-02-02 14:18:38
+ * @LastEditTime: 2024-02-02 17:00:01
  * @FilePath: \12.1软件模拟SPI驱动NOR FLASH\Drv_Common\norflash_drv.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -26,7 +18,7 @@
 #define SET_SPI_SCK()            gpio_bit_set(GPIOB, GPIO_PIN_13)
 #define CLR_SPI_SCK()            gpio_bit_reset(GPIOB, GPIO_PIN_13)
 
-#define READ_SPI_MISO()          gpio_input_port_get(GPIOB)
+#define READ_SPI_MISO()          gpio_input_bit_get(GPIOB,GPIO_PIN_14)
 
 #define SET_SPI_MOSI()           gpio_bit_set(GPIOB, GPIO_PIN_15)
 #define CLR_SPI_MOSI()           gpio_bit_reset(GPIOB, GPIO_PIN_15)
@@ -66,7 +58,7 @@ static void SPIInit(void)
     GpioInit();
 
     SET_SPI_NSS();
-    SET_SPI_SCK();
+    CLR_SPI_SCK();
 }
 
 static void SPIStart(void)
@@ -85,7 +77,6 @@ static uint8_t SPIReadWriteByte(uint8_t sendData)
 
     for (uint8_t i = 0; i < 8; i++)
     {
-        CLR_SPI_SCK();
         if((sendData & 0x80))
         {
             SET_SPI_MOSI();
@@ -99,7 +90,9 @@ static uint8_t SPIReadWriteByte(uint8_t sendData)
         SET_SPI_SCK();
         revData <<= 1;
         revData |= READ_SPI_MISO();
+        CLR_SPI_SCK();
     }
+	return revData;
 }
 
 
@@ -157,17 +150,169 @@ void ReadNorflashID(uint8_t *mid, uint16_t *did)
 
 /**
 ***********************************************************
-* @brief 
-* @param
+* @brief pointed address start reading pointed num of data
+* @param readAddr,readed address 
+* @param pBuffer,Array first address
+* @param len,nums of data
 * @return 
 ***********************************************************
 */
+void ReadNorflashData(uint32_t readAddr, uint8_t *pBuffer, uint32_t len)
+{
+    SPIStart();
+    SPIReadWriteByte(NORFLASH_READ_DATA);
+    SPIReadWriteByte((readAddr & 0xFF0000) >> 16);
+    SPIReadWriteByte((readAddr & 0x00FF00) >> 8);
+    SPIReadWriteByte(readAddr & 0xFF);
 
+    for (uint32_t i = 0; i < len; i++)
+    {
+        pBuffer[i] = SPIReadWriteByte(DUMMY_BYTE);
+    }
+    SPIStop();
+}
 
+static void EnableNorflashWrite(void)
+{
+    SPIStart();
+    SPIReadWriteByte(NORFLASH_WRITE_ENABLE);
+    SPIStop();
+}
 
+static void WaitNorflashWriteEnd(void)
+{
+    uint32_t Timeout = 100000;
+    SPIStart();
+    SPIReadWriteByte(NORFLASH_READ_STATUS_REGISTER_1);
 
+    while((SPIReadWriteByte(DUMMY_BYTE) & 0x01) == 0x01)
+    {
+        Timeout--;
+        if(Timeout == 0)
+        {
+            break;
+        }
+    }
+    SPIStop();
+}
 
+static void ProgramNorflashPage(uint32_t addr, uint8_t *pBuffer, uint32_t len)
+{
+    EnableNorflashWrite();
+    SPIStart();
+    SPIReadWriteByte(NORFLASH_PAGE_PROGRAM);
+    SPIReadWriteByte((addr & 0xFF0000) >> 16);
+    SPIReadWriteByte((addr & 0x00FF00) >> 8);
+    SPIReadWriteByte(addr & 0xFF);
 
+    while (len--)
+    {
+        SPIReadWriteByte(*pBuffer);
+        pBuffer++;
+    }
+
+    SPIStop();
+    WaitNorflashWriteEnd();
+}
+
+/**
+***********************************************************
+* @brief pointed address start reading pointed num of data
+* @param readAddr,readed address 
+* @param pBuffer,Array first address
+* @param len,nums of data
+* @return 
+***********************************************************
+*/
+void WriteNorflashData(uint32_t writeAddr, uint8_t *pBuffer, uint32_t len)
+{
+    uint8_t pageNum;
+    //release in paege offset
+    uint8_t addrOffset = writeAddr % NORFLASH_PAGE_SIZE;
+    
+    //previous spread
+    if (len > (NORFLASH_PAGE_SIZE - addrOffset))
+    {
+        //write full this page
+        ProgramNorflashPage(writeAddr, pBuffer, NORFLASH_PAGE_SIZE - addrOffset);
+        //align att page address
+        writeAddr += NORFLASH_PAGE_SIZE - addrOffset;
+        pBuffer += NORFLASH_PAGE_SIZE - addrOffset;
+        len -= NORFLASH_PAGE_SIZE - addrOffset;
+        pageNum = len  / NORFLASH_PAGE_SIZE;
+
+        while (pageNum--)
+        {
+            //write all the page
+            ProgramNorflashPage(writeAddr, pBuffer, NORFLASH_PAGE_SIZE);
+            writeAddr += NORFLASH_PAGE_SIZE;
+            pBuffer += NORFLASH_PAGE_SIZE;      
+            len -= NORFLASH_PAGE_SIZE; 
+        }
+        //write unfull page's remain
+        ProgramNorflashPage(writeAddr, pBuffer, len);
+    }
+    else    //Un align page
+    {
+         ProgramNorflashPage(writeAddr, pBuffer, len);
+    }
+}
+
+static void EraseNorflashSector(uint32_t eraseAddr)
+{
+    EnableNorflashWrite();
+    SPIStart();
+    SPIReadWriteByte(NORFLASH_SECTOR_ERASE_4KB);
+    SPIReadWriteByte((eraseAddr & 0xFF0000) >> 16);
+    SPIReadWriteByte((eraseAddr & 0x00FF00) >> 8);
+    SPIReadWriteByte(eraseAddr & 0xFF);
+    SPIStop();
+    WaitNorflashWriteEnd();
+}
+
+/**
+***********************************************************
+* @brief Erase continuous Block
+* @param eraseAdd,Erasing Sector's address
+* @param len,nums of data
+* @return 
+***********************************************************
+*/
+void EraseNorflashForWrite(uint32_t eraseAddr, uint32_t len)
+{
+    if(len == 0)
+    {
+        return;
+    }
+    uint8_t sectorNum;
+/*
+mod operation in a sector's offset,if eraseaddr is NORFLASH_SECTOR_SIZE's
+integer multiples operation's squartion is zero(0)
+*/
+    uint8_t addrOffset = eraseAddr % NORFLASH_SECTOR_SIZE;
+
+    if(len > (NORFLASH_SECTOR_SIZE - addrOffset)) //span the sector
+    {
+        EraseNorflashSector(eraseAddr); //erase this sector
+        eraseAddr += NORFLASH_SECTOR_SIZE - addrOffset; //align this sector
+        len -= NORFLASH_SECTOR_SIZE - addrOffset;
+        sectorNum = len / NORFLASH_SECTOR_SIZE;
+
+        while(sectorNum--)
+        {
+            EraseNorflashSector(eraseAddr);
+            eraseAddr += NORFLASH_SECTOR_SIZE;
+        }
+        if (len % NORFLASH_SECTOR_SIZE != 0)
+        {
+            EraseNorflashSector(eraseAddr);
+        }
+    }
+    else    //No sector
+    {
+        EraseNorflashSector(eraseAddr);
+    }
+}
 
 #define BUFFER_SIZE                4099
 #define FLASH_ADDRESS              0x000002
@@ -179,10 +324,34 @@ void NorflashDrvTest(void)
     uint8_t bufferRead[BUFFER_SIZE];
 
     printf("*************************\r\n");
-    printf("*******GD25QxxTestStart*********\r\n");
+    printf("*******GD25QxxTestStart*********");
     printf("*************************\r\n");
 
-    ReadNorflashID(&mid,&did);
-    printf("Norfalsh MID: 0x%X, DID: 0x%X\n",mid, did);
-}
+    ReadNorflashID(&mid, &did);
+    printf("Norfalsh MID: 0x%X, DID: 0x%X\n", mid, did);
 
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++)
+    {
+        bufferWrite[i] = i;
+        printf("0x%02X ", bufferWrite[i]);
+    }
+    printf("\r\n Starting write\r\n");
+
+    EraseNorflashForWrite(FLASH_ADDRESS, BUFFER_SIZE);
+    WriteNorflashData(FLASH_ADDRESS, bufferWrite, BUFFER_SIZE);
+
+    printf("starting read...\n");
+    ReadNorflashData(FLASH_ADDRESS, bufferRead, BUFFER_SIZE);
+
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++)
+    {
+        if (bufferRead[i] != bufferWrite[i])
+        {
+            printf("0x%02X ", bufferWrite[i]);
+            printf("Test break down,please check!!!\n");
+            return;
+        }
+        printf("0x%02X  ", bufferRead[i]);
+    }
+    printf("Pass - test!\n");
+}
