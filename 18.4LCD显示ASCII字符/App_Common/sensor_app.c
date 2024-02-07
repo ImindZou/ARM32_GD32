13698931e25c14b660e7c32763e36ec0c08e2bbd
zdh <2579941211@qq.com>
2024-02-07 15:58:17 +0800

    LCD显示ASCII码

---
+++ 18.4LCD显示ASCII字符/App_Common/sensor_app.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "sensor_drv.h"
+
+/**
+***********************************************************
+* @brief 传感器任务处理函数
+* @param 
+* @return 
+***********************************************************
+*/
+void SensorTask(void)
+{
+	SensorDrvProc();
+}
---
+++ 18.4LCD显示ASCII字符/Objects/queue.d
@@ -0,0 +1,3 @@
+.\objects\queue.o: Drv_Common\queue.c
+.\objects\queue.o: D:\Study\keil\UV4\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\queue.o: Drv_Common\queue.h
