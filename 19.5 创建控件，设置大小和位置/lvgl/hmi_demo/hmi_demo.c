64cf80439e359fbd5d5ea9a7bd2baf503ed845c7
zdh <2579941211@qq.com>
2024-03-02 22:02:57 +0800

    做一半

--- 19.5 创建控件，设置大小和位置/lvgl/hmi_demo/hmi_demo.c
+++ 19.5 创建控件，设置大小和位置/lvgl/hmi_demo/hmi_demo.c
@@ -74,7 +74,7 @@ void HmiDemo(void)
 
 #endif
 
-#if 1
+#if 0
 
 void HmiDemo()
 {
@@ -106,9 +106,114 @@ void HmiDemo()
     lv_obj_add_style(btn2,&style,0);
 }
 
-#endif // 1
+#endif
+
+#if 1
+#define STATE_ON    1
+#define STATE_OFF   0
+#define LED_NUM     3
+static uint8_t g_ledState[LED_NUM] = {STATE_OFF,STATE_OFF,STATE_OFF};
+static void Led1ClickedEventCb(lv_event_t *event)
+{
+    static lv_style_t style;
+    lv_style_init(&style);
+
+    lv_obj_t *ledCtrBtn = lv_event_get_target(event);
+
+    if (g_ledState[0] == STATE_OFF)
+    {
+        lv_style_set_bg_color(&style,lv_color_make(0xff, 0, 0));
+        g_ledState[0] = STATE_ON;
+    }
+    else
+    {
+        lv_style_set_bg_color(&style,lv_color_make(76, 151, 249));
+        g_ledState[0] = STATE_OFF;
+    }
+    lv_obj_add_style(ledCtrBtn, &style, 0);
+}
+
+static void Led2ClickedEventCb(lv_event_t *event)
+{
+    static lv_style_t style;
+    lv_style_init(&style);
+
+    lv_obj_t *ledCtrBtn = lv_event_get_target(event);
+
+    if (g_ledState[1] == STATE_OFF)
+    {
+        lv_style_set_bg_color(&style,lv_color_make(0xff, 0, 0));
+        g_ledState[1] = STATE_ON;
+    }
+    else
+    {
+        lv_style_set_bg_color(&style,lv_color_make(76, 151, 249));
+        g_ledState[1] = STATE_OFF;
+    }
+    lv_obj_add_style(ledCtrBtn, &style, 0);
+}
+
+static void Led3ClickedEventCb(lv_event_t *event)
+{
+    static lv_style_t style;
+    lv_style_init(&style);
+
+    lv_obj_t *ledCtrBtn = lv_event_get_target(event);
+
+    if (g_ledState[2] == STATE_OFF)
+    {
+        lv_style_set_bg_color(&style,lv_color_make(0xff, 0, 0));
+        g_ledState[2] = STATE_ON;
+    }
+    else
+    {
+        lv_style_set_bg_color(&style,lv_color_make(76, 151, 249));
+        g_ledState[2] = STATE_OFF;
+    }
+    lv_obj_add_style(ledCtrBtn, &style, 0);
+}
+
+void HmiDemo(void)
+{
+    lv_obj_set_style_bg_color(lv_scr_act(),lv_color_make(0, 0, 0), 0);
+
+    static lv_style_t style;
+    lv_style_init(&style);
 
 
+
+    //按键共享风格
+    lv_style_set_bg_color(&style,lv_color_make(76, 151, 249));
+    lv_style_set_bg_opa(&style,LV_OPA_50);
+    lv_style_set_radius(&style, 8);
+    lv_style_set_width(&style,lv_pct(80));
+    lv_style_set_height(&style,lv_pct(20));
+    lv_style_set_border_width(&style,0);
+    //按键1设置
+    lv_obj_t *ledCtrlBtn1  = lv_btn_create(lv_scr_act());
+    lv_obj_set_align(ledCtrlBtn1,LV_ALIGN_TOP_MID);
+    lv_obj_align(ledCtrlBtn1, LV_ALIGN_TOP_MID, 0, 10);
+    lv_obj_add_style(ledCtrlBtn1,&style,0);
+    lv_obj_add_event_cb(ledCtrlBtn1, Led1ClickedEventCb, LV_EVENT_CLICKED, NULL);
+
+    //按键2设置
+    lv_obj_t *ledCtrlBtn2  = lv_btn_create(lv_scr_act());
+    lv_obj_set_align(ledCtrlBtn2,LV_ALIGN_CENTER);
+    lv_obj_add_style(ledCtrlBtn2,&style,0);
+    lv_obj_add_event_cb(ledCtrlBtn2, Led2ClickedEventCb, LV_EVENT_CLICKED, NULL);
+
+    //按键3设置
+    lv_obj_t *ledCtrlBtn3  = lv_btn_create(lv_scr_act());
+    lv_obj_set_align(ledCtrlBtn3,LV_ALIGN_BOTTOM_MID);
+    lv_obj_align(ledCtrlBtn3, LV_ALIGN_BOTTOM_MID,0, -10);
+    lv_obj_add_style(ledCtrlBtn3,&style,0);
+    lv_obj_add_event_cb(ledCtrlBtn3, Led3ClickedEventCb, LV_EVENT_CLICKED, NULL);
+}
+
+
+
+
+#endif
 
 
 
